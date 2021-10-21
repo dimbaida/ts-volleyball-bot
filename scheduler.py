0e98d39fa95477989c7cac06ee5c796ddd4026b2
Dim <dimbaida@gmail.com>
2021-10-21 22:13:49 +0300

    check if there are upcoming events

--- scheduler.py
+++ scheduler.py
@@ -10,23 +10,25 @@ def send_event_reminder() -> None:
     Sends the reminder to players who didn't yet make any decision on the nearest upcoming event
     """
     print("[INFO] Starting the scheduled event reminder")
-    event = db.upcoming_events()[0]
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    btn_01 = telebot.types.InlineKeyboardButton('YES', callback_data=f'LIST_EVENTS>>EVENT::{event.id}:yes')
-    btn_02 = telebot.types.InlineKeyboardButton('NO', callback_data=f'LIST_EVENTS>>EVENT::{event.id}:no')
-    keyboard.row(btn_01, btn_02)
-    players = db.get_active_players()
-    bot = telebot.TeleBot(config.bot_token)
-    for player in players:
-        if player.check_attendance(event.date) is None:
-            print(f'[INFO] Sending reminder on {event.date} to {player.name} {player.lastname}')
-            event_text = f"Напоминание: {event.icon} {event.date_formatted}"
-            if event.note:
-                event_text += f"\n{event.note}"
-            bot.send_message(player.telegram_id,
-                             f'<code>{event_text}</code>',
-                             reply_markup=keyboard,
-                             parse_mode='HTML')
+    events = db.upcoming_events()
+    if events:
+        event = events[0]
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        btn_01 = telebot.types.InlineKeyboardButton('YES', callback_data=f'LIST_EVENTS>>EVENT::{event.id}:yes')
+        btn_02 = telebot.types.InlineKeyboardButton('NO', callback_data=f'LIST_EVENTS>>EVENT::{event.id}:no')
+        keyboard.row(btn_01, btn_02)
+        players = db.get_active_players()
+        bot = telebot.TeleBot(config.bot_token)
+        for player in players:
+            if player.check_attendance(event.date) is None:
+                print(f'[INFO] Sending reminder on {event.date} to {player.name} {player.lastname}')
+                event_text = f"Напоминание: {event.icon} {event.date_formatted}"
+                if event.note:
+                    event_text += f"\n{event.note}"
+                bot.send_message(player.telegram_id,
+                                 f'<code>{event_text}</code>',
+                                 reply_markup=keyboard,
+                                 parse_mode='HTML')
 
 
 def send_birthday_reminder() -> None:

7e77a6345daf9e54e3a6a748093b67485c1f6def
dimbaida Office PC <dimbaida@gmail.com>
2022-10-27 15:23:03 +0300

    logging config for scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -5,6 +5,10 @@ import datetime
 from common_constants import ICONS
 import db_commands as db
 
+logging.basicConfig(level=logging.INFO,
+                    datefmt='%d.%m.%y|%H:%M:%S',
+                    format='[%(asctime)s] %(name)s:%(levelname)s >> %(message)s')
+
 
 def send_event_reminder() -> None:
     """

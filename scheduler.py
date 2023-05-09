61d809131d56fc0d48479d1993df30e24204eebd
dim macbook <dimbaida@gmail.com>
2023-05-09 11:20:12 +0300

    scheduler test

--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,10 @@
-import os
 import logging
 import telebot
 import datetime
 from common_constants import ICONS, BOT_TOKEN, TS_GROUP_ID, MASTER_TG_ID
 import db_commands as db
 
-# the full filepath is needed for crontab job
-dir_name = os.path.dirname(os.path.realpath(__file__))
-filename = os.path.join(dir_name, '.log')
-
-logging.basicConfig(filename=filename,
+logging.basicConfig(filename='.log',
                     level=logging.INFO,
                     format='%(asctime)s :: %(levelname)s >> %(message)s',
                     datefmt='%d-%m-%Y %H:%M:%S')

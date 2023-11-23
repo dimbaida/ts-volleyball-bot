6720066be2a98257c0eb58247c5dd7052841cb2b
dim-officePC <dimbaida@gmail.com>
2023-11-23 11:24:10 +0200

    logs cleaner

---
+++ log_cleaner.sh
@@ -0,0 +1,9 @@
+file="bot.log"
+line_count=$(wc -l < "$file")
+threshold=1000
+
+if [ "$line_count" -gt "$threshold" ]; then
+    lines_to_delete=$((line_count - threshold))
+    sed -i "1,${lines_to_delete}d" "$file"
+fi
+
